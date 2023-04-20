15abe38c42b4d55f9a1ac4ff19a6991452cb5213
Héctor Fonzalida <hectorfonzalida68@gmail.com>
2023-04-19 22:24:15 -0300

    Semana 1 - Video 1.3
    Procesar clases de excepción más específicas

--- Python/Semana 1 (03-04 al 07-04)/Excepciones/Leccion01/manejo_excepciones.py
+++ Python/Semana 1 (03-04 al 07-04)/Excepciones/Leccion01/manejo_excepciones.py
@@ -1,10 +1,15 @@
 resultado = None
-a = '10'
+a = 7
 b = 0
 try:  
     resultado = a / b #modificamos
+
+except TypeError as e:
+    print(f'TypeError - Ocurrió un error: {type(e)}')
 except ZeroDivisionError as e:
     print(f'Ocurrio un error: {e}')
+except Exception as e:
+    print(f'Exception - Ocurrió un error: {type(e)}')
 
 print(f'El resultado es: {resultado}')
 print('seguimos...')
