8017c50f3784dd81dac73ce41d4485982980041f
Nicolás Machuca <112595518+Nico25Mac@users.noreply.github.com>
2023-04-22 12:21:21 -0300

    Semana 2 - Video 1.2
    Especificar el juego de caracteres de un archivo de texto:
    Agregamos un juego de caracteres para que el programa incluya los acentos y también agregamos 3 líneas de código.

--- Python/Semana 2 (10-04 al 14-04)/Archivos/Leccion02/manejo_archivos.py
+++ Python/Semana 2 (10-04 al 14-04)/Archivos/Leccion02/manejo_archivos.py
@@ -1,11 +1,13 @@
 
 # Declaramos una variable
 try:
-    archivo = open('prueba.txt', 'w') # La w es de write que significa escribir
+    archivo = open('prueba.txt', 'w', encoding='utf8') # La w es de write que significa escribir
     archivo.write('Programamos con diferentes tipos de archivos, ahora en txt.\n')
+    archivo.write('Los acentos son importantes para las palabras\n')
+    archivo.write('como por ejemplo: acción, ejecución y producción\n')
     archivo.write('Con esto terminamos')
 except Exception as e:
     print(e)
 finally:    # siempre se ejecuta
     archivo.close() # con esto se debe cerrar el archivo
-
+#   archivo.write('Todo quedo perfecto'): este es un error
