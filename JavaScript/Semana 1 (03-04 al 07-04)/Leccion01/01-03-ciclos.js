3a48b7ce906f25d4e0c6ee57b73675cd7b7ff22a
Lucas Mendez <112028014+LucasMendez11@users.noreply.github.com>
2023-04-24 12:24:22 -0300

    Update 01-03-ciclos.js

--- JavaScript/Semana 1 (03-04 al 07-04)/Leccion01/01-03-ciclos.js
+++ JavaScript/Semana 1 (03-04 al 07-04)/Leccion01/01-03-ciclos.js
@@ -35,6 +35,6 @@ for(let contando = 0; contando <= 10; contando++){
     if(contando % 2 !== 0){
     continue; //ir a la siguiente iteración
          }
-         console.log(contando): 0, 2, 4, 6, 8, 10
+         console.log(contando);
 }
-console.log("Terminar ciclo"); Termina el ciclo
+console.log("Terminar ciclo"); //Termina el ciclo
