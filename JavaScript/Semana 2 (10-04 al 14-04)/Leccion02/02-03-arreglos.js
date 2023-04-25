99ec87846eae31dca7716a6600dc39910bd3c796
Héctor Fonzalida <hectorfonzalida68@gmail.com>
2023-04-25 16:19:30 -0300

    Arreglos - 1.3 Modificamos los elementos

--- JavaScript/Semana 2 (10-04 al 14-04)/Leccion02/02-03-arreglos.js
+++ JavaScript/Semana 2 (10-04 al 14-04)/Leccion02/02-03-arreglos.js
@@ -9,3 +9,19 @@ console.log(autos[2]);
 for(let i = 0; i < autos.length; i ++){
     console.log(i+' : '+autos[i]);
 }
+
+// Modificamos los elementos del arreglo
+autos[1] = 'Volvo';
+console.log(autos[1]);
+
+// Agregamos nuevos valores al arreglo
+autos.push('Audi'); // Agregamos el elemento al final del arreglo
+console.log(autos);
+
+// Otras formas de agregar elementos al arreglo
+autos[autos.length] = 'Porsche'
+console.log(autos);
+
+// Tercera forma de agregar elementos teniendo CUIDADO
+autos[6] = 'Renault';
+console.log(autos);
