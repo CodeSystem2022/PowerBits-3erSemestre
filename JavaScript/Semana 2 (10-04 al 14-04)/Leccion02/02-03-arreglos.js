590e5e538e2969eec01663d5d07283590a8d6a23
Lucas Mendez <112028014+LucasMendez11@users.noreply.github.com>
2023-04-24 21:25:58 -0300

    Semana 2 - Video 1.2
    Recorremos los elementos de un arreglo

--- JavaScript/Semana 2 (10-04 al 14-04)/Leccion02/02-03-arreglos.js
+++ JavaScript/Semana 2 (10-04 al 14-04)/Leccion02/02-03-arreglos.js
@@ -1,3 +1,11 @@
 // let autos = new Array('Ferrari', 'Renault', 'BMW'); // Esta es la sintaxis vieja
 const autos =['Ferrari', 'Renault', 'BMW'];
 console.log(autos);
+
+// Recorremos los elementos de un arreglo
+console.log(autos[0]);
+console.log(autos[2]);
+
+for(let i = 0; i < autos.length; i ++){
+    console.log(i+' : '+autos[i]);
+}
