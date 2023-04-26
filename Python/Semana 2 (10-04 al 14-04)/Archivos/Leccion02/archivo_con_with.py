a7a37821464801fdde65dd54d3feba730794653a
juaniM4c <juanimachuca1@gmail.com>
2023-04-26 19:08:28 -0300

    Semana 2 - Video 1.5
    1.5 Uso de with, archivos y contexto Manager Parte 1

---
+++ Python/Semana 2 (10-04 al 14-04)/Archivos/Leccion02/archivo_con_with.py
@@ -0,0 +1,8 @@
+# MANEJO DE CONTEXTO WITH: sintaxis simplificada, abre y cierra el archivo
+with open("prueba.txt","r",encoding="utf8") as archivo:
+    print(archivo.read())
+# No hace falta ni el try, ni el finally
+# En el contexto de with lo que se ejecuta de manera automatica
+# Utiliza diferentes metodos: __enter__ este es el que abre
+# ahora el siguiente metodo es el que cierra: __exit__
+
