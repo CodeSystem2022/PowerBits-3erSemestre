3b850a1428319ee160680fb2893edf83ca110fb8
Nico25Mac <112595518+Nico25Mac@users.noreply.github.com>
2023-04-26 13:27:32 -0300

    Semana 3 - Video 1.2
    Manejo de Enumeraciones (enum)
    
    Creamos la clase Dias y la clase TestEnumeraciones.

---
+++ Java/Semana 3 (10-04 al 14-04)/Leccion02/Enumeraciones/src/enumeraciones/Dias.java
@@ -0,0 +1,11 @@
+package enumeraciones;
+
+public enum Dias {
+    LUNES,
+    MARTES,
+    MIERCOLES,
+    JUEVES,
+    VIERNES,
+    SABADO,
+    DOMINGO;
+}
---
+++ Java/Semana 3 (10-04 al 14-04)/Leccion02/Enumeraciones/src/test/TestEnumeraciones.java
@@ -0,0 +1,9 @@
+package test;
+
+import enumeraciones.Dias;
+
+public class TestEnumeraciones {
+    public static void main(String[] args) {
+        System.out.println("Dia 1: "+Dias.LUNES); 
+    }  
+}
