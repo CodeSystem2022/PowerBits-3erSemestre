a8ab0762abf926bab081908131c8445cf57f63bc
Lucas Mendez <112028014+LucasMendez11@users.noreply.github.com>
2023-04-27 00:50:51 -0300

    update Leccion02
    corregimos algunos errores

--- Python/Semana 2 (10-04 al 14-04)/Archivos/Leccion02/leer_archivo.py
+++ Python/Semana 2 (10-04 al 14-04)/Archivos/Leccion02/leer_archivo.py
@@ -3,7 +3,18 @@ archivo = open('prueba.txt', 'r',
 # print(archivo.read())
 # print(archivo.read(16))
 # print(archivo.read(10)) # continuamos desde la linea anterior
-print(archivo.readline())
-print(archivo.readline())
+# print(archivo.readline())
+# print(archivo.readline())
 
+# vamos a iterar el archivo, cada una de las lineas
+# for linea in archivo:
+# print(linea): iteramos todsos los elementos del archivo
+# print(archvio readlines()[11]): # accedemos al archivo como si fuera una lista
 
+# Anexamos informacion, copiamos a otro
+archivo2 = open('copia.txt','a', encoding='utf8' )
+archivo2.write(archivo.read())
+archivo.close() # cerramos el primer archivo
+archivo2.close() # cerramos el segundo archivo
+
+print('Se ha terminado el proceso de leer y copiar archivos')
