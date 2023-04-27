8fb5d8f401ac06a7bd4c840f1fe36a8b2fa2137a
Héctor Fonzalida <hectorfonzalida68@gmail.com>
2023-04-27 15:34:22 -0300

    Semana 3 - Video 1.2
    Creación de la clase: Película

---
+++ Python/Semana 3 (17-04 al 21.04)/Leccion03/catalogo_peliculas/dominio/Pelicula.py
@@ -0,0 +1,14 @@
+class Pelicula:
+    def __init__(self, nombre):
+        self.nombre = nombre
+
+    def __str__(self):
+        return f'Pelicula: {self._nombre}'
+
+    @property
+    def nombre(self):
+        return self._nombre
+
+    @nombre.setter
+    def nombre(self, nombre):
+        self.nombre = nombre
