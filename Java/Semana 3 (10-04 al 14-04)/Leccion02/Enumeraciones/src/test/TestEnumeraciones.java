e14d1dedb8d9634b31b79f1c7ede1ee31eb5ec08
Lucas Mendez <112028014+LucasMendez11@users.noreply.github.com>
2023-04-27 11:19:44 -0300

    Revert "Semana2 - Video 1.3"
    This reverts commit 857e5e746c7b17259bc03976141b8bb03ab9a47a.

--- Java/Semana 3 (10-04 al 14-04)/Leccion02/Enumeraciones/src/test/TestEnumeraciones.java
+++ Java/Semana 3 (10-04 al 14-04)/Leccion02/Enumeraciones/src/test/TestEnumeraciones.java
@@ -1,52 +1,9 @@
 package test;
 
-import enumeraciones.Continentes;
 import enumeraciones.Dias;
 
 public class TestEnumeraciones {
     public static void main(String[] args) {
-        // System.out.println("Dia 1: "+Dias.LUNES); 
-        // indicarDiaSemana(Dias.LUNES); // Las enumeraciones se tratan como cadenas
-        // Ahora no se deben utilizar comillas, se accede a traves del operador de punto
-        
-        System.out.println("Continente No. 4: "+Continentes.AMERICA);
-        System.out.println("No. de paises en el 4to. continente: "
-                +Continentes.AMERICA.getPaises());
-        System.out.println("No. de habitantes en el 4to. continente: "
-                +Continentes.AMERICA.getHabitantes());
-        
+        System.out.println("Dia 1: "+Dias.LUNES); 
     }  
-    
-    
-    private static void indicarDiaSemana(Dias dias){
-        switch(dias){
-            case LUNES:
-                System.out.println("Primer dia de la semana");
-                break;
-            case MARTES:
-                System.out.println("Segundo dia de la semana");
-                break;
-                // Agregar todos los dias de la semana
-                // Agregar default
-            case MIERCOLES:
-                System.out.println("Tercer dia de la semana");
-                break;
-            case JUEVES:
-                System.out.println("Cuarto dia de la semana");
-                break;
-            case VIERNES:
-                System.out.println("Quinto dia de la semana");
-                break;
-            case SABADO:
-                System.out.println("Sexto dia de la semana");
-                break;
-            case DOMINGO:
-                System.out.println("Septimo dia de la semana");
-                break;
-            default:
-                System.out.println("Error");
-        }
-        
-    }
 }
-
