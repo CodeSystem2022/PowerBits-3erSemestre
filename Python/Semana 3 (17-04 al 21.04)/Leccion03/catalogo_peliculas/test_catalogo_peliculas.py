56fa0c93b700e3df1a3068a0173ee6685594b136
IviiGonzalez <112676298+IviiGonzalez@users.noreply.github.com>
2023-04-28 09:53:37 -0300

    Update Semana3 - Video 1.5

--- Python/Semana 3 (17-04 al 21.04)/Leccion03/catalogo_peliculas/test_catalogo_peliculas.py
+++ Python/Semana 3 (17-04 al 21.04)/Leccion03/catalogo_peliculas/test_catalogo_peliculas.py
@@ -1,3 +1,5 @@
+from dominio.Pelicula import Pelicula
+from servicio.catalogo_peliculas import CatalogoPeliculas as cp
 opcion = None
 while opcion != 4:
      try:
@@ -7,7 +9,16 @@ while opcion != 4:
         print("3. Eliminar catalogo de peliculas")
         print("4. Salir")
         opcion = int(input("Digite una opcion de menu (1-4): "))
-     except Exception as e:
+        if opcion == 1:
+         nombre_pelicula = input('Digite el nombre de la pelicula')
+         pelicula = Pelicula(nombre_pelicula)
+         cp.agregar_peliculas(nombre_pelicula)
+        elif opcion == 2:
+         cp.listar_peliculas()
+        elif opcion == 3:
+         cp.eliminar_peliculas()
+
+     except Execption as e:
         print(f"Ocurrio un error de tipo: {e}")
         opcion = None
      else:
