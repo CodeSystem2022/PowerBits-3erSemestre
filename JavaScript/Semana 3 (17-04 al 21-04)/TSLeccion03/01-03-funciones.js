12e37ce67bdc5cd3dc77bf8a4f7771fa6b089575
Antonella arraez <antooarraez2001@gmail.com>
2023-04-30 10:17:17 -0300

    Semana3 - Video 1.2
    Palabra return.

--- JavaScript/Semana 3 (17-04 al 21-04)/TSLeccion03/01-03-funciones.js
+++ JavaScript/Semana 3 (17-04 al 21-04)/TSLeccion03/01-03-funciones.js
@@ -1,8 +1,12 @@
 miFuncion(8, 2); //Esto se le conoce como hoisting
 
 function miFuncion(a, b){
-    console.log("Sumamos: "+ (a + b));
+    //console.log("Sumamos: "+ (a + b));
+    return a + b;
 }
 
 //Llamando la función
 miFuncion(5, 4);
+
+let resultado = miFuncion(6, 7);
+console.log(resultado);
