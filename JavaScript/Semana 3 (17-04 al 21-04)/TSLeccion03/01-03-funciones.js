3c7a85f554081ff8669c198c6271abee72a49134
Nicolás Machuca <112595518+Nico25Mac@users.noreply.github.com>
2023-05-02 21:29:25 -0300

    Semana 3 - Video 1.8
    Concepto hoisting

--- JavaScript/Semana 3 (17-04 al 21-04)/TSLeccion03/01-03-funciones.js
+++ JavaScript/Semana 3 (17-04 al 21-04)/TSLeccion03/01-03-funciones.js
@@ -46,3 +46,14 @@ let sumar = function(a = 4, b = 8){
 }
 resultado = sumar(3,2,9);
 console.log(resultado);
+
+//Sumar todos los argumentos
+let respuesta = sumarTodo(5, 4, 13, 10, 9);
+console.log(respuesta);
+function sumarTodo(){
+    let suma = 0;
+    for(let i = 0; i < arguments.length; i++){
+        suma += arguments[i]; //Arguments es para arreglos
+    }
+    return suma;
+}
