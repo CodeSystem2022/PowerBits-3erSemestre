4c2fba5f6580e0b0afb12d178d76f46a1959dc2e
Lucas Mendez <112028014+LucasMendez11@users.noreply.github.com>
2023-05-05 10:54:49 -0300

    Semana 4 - Video 1.3
    Modificadores de acceso public

---
+++ Java/Semana4 (24-04 al 28-04)/Leccion03/ModificadoresAcceso/src/paquete1/Clase1.java
@@ -0,0 +1,14 @@
+
+package paquete1;
+
+
+public class Clase1 {
+    public String atributoPublic = "Valor atributo public";
+    
+    public Clase1(){
+        System.out.println("Constructor public");
+    }
+    public void metodoPublico(){
+        System.out.println("Metodo public");
+    }
+}
---
+++ Java/Semana4 (24-04 al 28-04)/Leccion03/ModificadoresAcceso/src/test/TestModificadoresAcceso.java
@@ -0,0 +1,12 @@
+
+package test;
+
+import paquete1.Clase1;
+
+public class TestModificadoresAcceso {
+    public static void main(String[] args) {
+        Clase1 clase1 = new Clase1();
+        System.out.println("clase1 = " + clase1.atributoPublic);
+        clase1.metodoPublico();
+    }
+}
