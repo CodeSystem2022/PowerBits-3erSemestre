f3966b56e186dc75ca1087f694a256c60ade999a
Antonella arraez <antooarraez2001@gmail.com>
2023-05-08 19:11:16 -0300

    Video - 1.6
     Ejecutamos desde el navegador

--- JavaScript/Semana 4 (24-04 al 08-04)/01-04-Objetos.js
+++ JavaScript/Semana 4 (24-04 al 08-04)/01-04-Objetos.js
@@ -1,7 +1,6 @@
 let x = 10; //variable de tipo primitiva
 console.log(x.length);
-console.log('Tipos Primitivos');
-
+console.log('Tipos primitivos');
 //Objeto
 let persona = {
     nombre: 'Carlos',
@@ -18,23 +17,25 @@ console.log('Apellido: '+persona.apellido);
 console.log('Su email es: '+persona.email);
 console.log('De la edad de '+persona.edad +' años');
 console.log(persona.nombreCompleto());
-
+console.log('Ejecutando con un objeto');
 let persona2 = new Object();//Debe crear un nuevo objeto en memoria
 persona2.nombre = "Juan";
 persona2.direccion = "Salada 14";
 persona2.telefono = "45134234512";
 console.log(persona2.telefono);
-
+console.log('Creamos un nuevo objeto');
 console.log(persona['apellido']); //Accedemos como si fuera un arreglo
-
+console.log('Usamos el ciclo for in');
 //for in y accedemos al objeto como si fuera un arreglo
 for (propiedad in persona) {
     console.log(propiedad);
     console.log(persona[propiedad]);
     
 }
-
+console.log('Cambiamos y eliminamos un error');
 persona.apellida = 'Betancud'; //Cambiamos dinamicamente un valor del objeto
 delete persona.apellida; //Eliminamos el error
 console.log(persona);
+
+
 
