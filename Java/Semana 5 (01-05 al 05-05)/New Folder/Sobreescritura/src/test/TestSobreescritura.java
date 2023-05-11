38beb5858bd062018735f10d41926829fe32c024
Nico25Mac <112595518+Nico25Mac@users.noreply.github.com>
2023-05-11 14:24:06 -0300

    Semana 5 - Video 1.3  parte 1
    Polimorfismo Parte 1.
    El nombre correspondiente al commit anterior a este seria *Semana 5 - Video 1.2  parte 2*

--- Java/Semana 5 (01-05 al 05-05)/New Folder/Sobreescritura/src/test/TestSobreescritura.java
+++ Java/Semana 5 (01-05 al 05-05)/New Folder/Sobreescritura/src/test/TestSobreescritura.java
@@ -1,12 +1,20 @@
 
 package test;
 
-import domain.Gerente;
+import domain.*;
 
 
 public class TestSobreescritura {
     public static void main(String[] args) {
+        Empleado empleado1 = new Empleado("Juan", 1000);
+        imprimir(empleado1);
+        //System.out.println("empleado1 = " + empleado1.obtenerDetalles());
         Gerente gerente1 = new Gerente("Jose", 5000, "Sistemas");
-        System.out.println("gerente1 = " + gerente1.obtenerDetalles());
+        imprimir(gerente1);
+        //System.out.println("gerente1 = " + gerente1.obtenerDetalles());
+    }
+    
+    public static void imprimir(Empleado empleado){
+        System.out.println("empleado = " + empleado.obtenerDetalles());
     }
 }
