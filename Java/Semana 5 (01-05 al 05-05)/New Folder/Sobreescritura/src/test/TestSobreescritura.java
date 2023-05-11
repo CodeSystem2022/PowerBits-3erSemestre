e8721d9b18ece4b4f4360f7798e8e0e3ed1da103
NadiaGonzalez27 <nadiagonzalez206@gmail.com>
2023-05-11 16:11:32 -0300

    Semana 5 - Video 1.4

--- Java/Semana 5 (01-05 al 05-05)/New Folder/Sobreescritura/src/test/TestSobreescritura.java
+++ Java/Semana 5 (01-05 al 05-05)/New Folder/Sobreescritura/src/test/TestSobreescritura.java
@@ -6,15 +6,16 @@ import domain.*;
 
 public class TestSobreescritura {
     public static void main(String[] args) {
-        Empleado empleado1 = new Empleado("Juan", 1000);
+        Empleado empleado1 = new Empleado("Juan", 10000);
         imprimir(empleado1);
         //System.out.println("empleado1 = " + empleado1.obtenerDetalles());
-        Gerente gerente1 = new Gerente("Jose", 5000, "Sistemas");
-        imprimir(gerente1);
+        empleado1 = new Gerente("Jose", 5000, "Sistemas");
+        imprimir(empleado1);
         //System.out.println("gerente1 = " + gerente1.obtenerDetalles());
     }
     
     public static void imprimir(Empleado empleado){
-        System.out.println("empleado = " + empleado.obtenerDetalles());
+        String detalles = empleado.obtenerDetalles();
+        System.out.println("detalles = " + detalles);
     }    
 }
