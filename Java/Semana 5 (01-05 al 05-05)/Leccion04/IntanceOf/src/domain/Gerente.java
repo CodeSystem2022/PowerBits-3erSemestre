0a2e5039b317c573ca2256d771e2202795498bf1
NadiaGonzalez27 <nadiagonzalez206@gmail.com>
2023-05-12 10:32:51 -0300

    Semana 5 - Video 1.6 parte 2

--- Java/Semana 5 (01-05 al 05-05)/Leccion04/IntanceOf/src/domain/Gerente.java
+++ Java/Semana 5 (01-05 al 05-05)/Leccion04/IntanceOf/src/domain/Gerente.java
@@ -14,4 +14,12 @@ public class Gerente extends Empleado{
     public String obtenerDetalles(){
         return super.obtenerDetalles()+", Departamento: "+this.departamento;
     }
+    
+    public String getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
 }
--- Java/Semana 5 (01-05 al 05-05)/Leccion04/IntanceOf/src/test/TestInstanceof.java
+++ Java/Semana 5 (01-05 al 05-05)/Leccion04/IntanceOf/src/test/TestInstanceof.java
@@ -7,17 +7,23 @@ import domain.*;
 public class TestInstanceof {
     public static void main(String[] args) {
         Empleado empleado1 = new Empleado("Juan", 10000);
-        //determinarTipo(empleado1);
-        empleado1 = new Gerente("Jose", 5000, "Sistemas");
         determinarTipo(empleado1);
+        empleado1 = new Gerente("Jose", 5000, "Sistemas");
+        //determinarTipo(empleado1);
     }
     
     public static void determinarTipo(Empleado empleado){
         if(empleado instanceof Gerente){
             System.out.println("Es de tipo Gerente");
+            Gerente gerente = (Gerente)empleado;
+            //(Gerente)empleado.getDepartamento();
+            System.out.println("gerente = " + gerente.getDepartamento());
+            
         }
         else if(empleado instanceof Empleado){
             System.out.println("Es de tipo Empleado");
+            //Gerente gerente = (Gerente)empleado;
+            //System.out.println("gerente = " + gerente.getDepartamento());
         }
         else if(empleado instanceof Object){
             System.out.println("Es de tipo Object");
