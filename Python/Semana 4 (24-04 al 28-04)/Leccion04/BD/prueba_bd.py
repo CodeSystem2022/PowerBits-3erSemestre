8e2d4b5e95de969a45c927a35a12de299a40e84b
Antonella arraez <antooarraez2001@gmail.com>
2023-05-17 15:27:04 -0300

    Semana 5 - Video 5.2
     Función fetchone en psycopg2

--- Python/Semana 4 (24-04 al 28-04)/Leccion04/BD/prueba_bd.py
+++ Python/Semana 4 (24-04 al 28-04)/Leccion04/BD/prueba_bd.py
@@ -4,9 +4,10 @@ conexion = psycopg2.connect( user='postgres', password='admin', host='127.0.0.1'
 try:
     with conexion:
         with conexion.cursor() as cursor:
-            sentencia = 'SELECT * FROM persona'
-            cursor.execute(sentencia) # De esta manera ejecutamos la sentencia.
-            registros = cursor.fetchall() # Recuperamos todos los registros que serán una lista.
+            sentencia = 'SELECT * FROM persona WHERE id_persona = %s' # Placeholder
+            id_persona = input('Digite un número para el id_persona: ')
+            cursor.execute(sentencia, (id_persona,)) # De esta manera ejecutamos la sentencia.
+            registros = cursor.fetchone() # Recuperamos todos los registros que serán una lista.
             print(registros)
 
 except Exception as e:
