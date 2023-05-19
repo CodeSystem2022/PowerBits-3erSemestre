083b3122dc3176f2a2781d7af13e83eb4d86ff35
Facundo Rios <112650077+jfacundorios@users.noreply.github.com>
2023-05-19 19:27:34 -0300

    Semana 5-5.5 Clases Abstractas (abstract) (Parte 3)

---
+++ Java/Semana 6 ( 08-05 al 12-05)/Leccion05/ClasesAbstractas/ClaseAbstracta.java
@@ -0,0 +1,12 @@
+
+package test
+        
+import domain.*;
+
+public class TestAbstractas {
+    public static void main(String[] args) {
+        FiguraGeometrica figura = new Rectangulo("Rectangulo");
+    figura.dibujar();
+    }
+    
+             
