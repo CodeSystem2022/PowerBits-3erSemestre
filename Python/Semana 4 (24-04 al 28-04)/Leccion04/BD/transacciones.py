be5f70a4eecc55de94f119c77446221a2584fd17
Lucas Mendez <112028014+LucasMendez11@users.noreply.github.com>
2023-05-22 10:50:24 -0300

    Semana 6 - Video 6.2 parte 2
    Manejo de transacciones (Manualmente):

--- Python/Semana 4 (24-04 al 28-04)/Leccion04/BD/transacciones.py
+++ Python/Semana 4 (24-04 al 28-04)/Leccion04/BD/transacciones.py
@@ -2,14 +2,16 @@ import psycopg2 as bd # Esto es para poder conectarnos con Postgre
 
 conexion = bd.connect( user='postgres', password='admin', host='127.0.0.1', port='5432', database='test_bd')
 try:
+    # conexion.autocommit = False # esto directamente no debería estar
     cursor = conexion.cursor()
     sentencia = 'INSERT INTO persona(nombre, apellido, email) VALUES(%s, %s, %s)'
     valores = ('Maria', 'Esparza', 'mesparzaœmail.com')
     cursor.execute(sentencia, valores)
-    conexion.commit()  # Realizamos el commit manualmente
-    print('Termina la transaccion')
+    conexion.commit()  # Hacemos el commit manualmente
+    print('Termina la transacción')
 
 except Exception as e:
-    print(f'Ocurrió un error: {e}')
+    conexion.rollback()
+    print(f'Ocurrió un error, se hizo un rollback: {e}')
 finally:
     conexion.close()
