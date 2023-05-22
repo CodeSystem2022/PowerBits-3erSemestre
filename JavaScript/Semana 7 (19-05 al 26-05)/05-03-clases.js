d6738952626b1f1c0ac504b1f56b920a39bdf949
Lucas Mendez <112028014+LucasMendez11@users.noreply.github.com>
2023-05-22 11:06:10 -0300

    Clase 6 - 6.2 Metodo Get y Set - Video 3

--- JavaScript/Semana 7 (19-05 al 26-05)/05-03-clases.js
+++ JavaScript/Semana 7 (19-05 al 26-05)/05-03-clases.js
@@ -1,11 +1,15 @@
 class Persona{
     constructor(nombre, apellido){
-        this.nombre = nombre;
-        this.apellido = apellido;
+        this._nombre = nombre;
+        this._apellido = apellido;
+    }
+
+    get nombre(){
+        return this._nombre;
     }
 }
 
 let persona1 = new Persona('Martín', 'Perez');
-console.log(persona1);
+console.log(persona1.nombre);
 let persona2 = new Persona('Carlos', 'Lara');
-console.log(persona2);
+console.log(persona2.nombre);
