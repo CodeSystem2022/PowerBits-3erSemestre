73a96b156963b27a0061f8c49c128e02c5d539c0
Antonella arraez <antooarraez2001@gmail.com>
2023-05-21 21:02:49 -0300

    Semana 7 - Video 6.1(Parte 2)
     Sintaxis de Clases en JavaScript: (Parte 2)

---
+++ JavaScript/Semana 7 (19-05 al 26-05)/05-03-clases.js
@@ -0,0 +1,11 @@
+class Persona{
+    constructor(nombre, apellido){
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+}
+
+let persona1 = new Persona('Martín', 'Perez');
+console.log(persona1);
+let persona2 = new Persona('Carlos', 'Lara');
+console.log(persona2);
