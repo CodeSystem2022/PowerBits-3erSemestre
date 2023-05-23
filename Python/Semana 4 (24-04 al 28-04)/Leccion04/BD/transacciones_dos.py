fb8bde9f0c788a53953e9acebfb434ef385101ad
juaniM4c <juanimachuca1@gmail.com>
2023-05-23 14:16:47 -0300

    Semana 7 - Video 6.2 (Parte 4)
    Manejo de transacciones (Manualmente): Parte  4

--- Python/Semana 4 (24-04 al 28-04)/Leccion04/BD/transacciones_dos.py
+++ Python/Semana 4 (24-04 al 28-04)/Leccion04/BD/transacciones_dos.py
@@ -5,11 +5,11 @@ try:
     conexion.autocommit = False # esto directamente no debería estar, inicia la transacción
     cursor = conexion.cursor()
     sentencia = 'INSERT INTO persona(nombre, apellido, email) VALUES(%s, %s, %s)'
-    valores = ('Carlos', 'Lara', 'clara@mail.com')
+    valores = ('Jorge', 'Prol', 'jprol@mail.com')
     cursor.execute(sentencia, valores)
 
     sentencia = 'UPDADTE persona SET nombre=%s, apellido=%s, email=%s WHERE id_persona=%s'
-    valores = ('Juan', 'Juarez', 'jcjuarez@mail.com', 1)
+    valores = ('Juan Carlos', 'Perez', 'jcperez@mail.com', 1)
     cursor.execute(sentencia, valores)
 
     conexion.commit()  # Hacemos el commit manualmente, se cierra la transacción
