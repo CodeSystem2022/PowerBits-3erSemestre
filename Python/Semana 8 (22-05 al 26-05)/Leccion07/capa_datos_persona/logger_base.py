218be77abef4e58e4c664e4c9b51b7e77435fa71
Antonella arraez <antooarraez2001@gmail.com>
2023-05-29 20:39:49 -0300

    7.2 - Manejo de logging - (Video 2)

--- Python/Semana 8 (22-05 al 26-05)/Leccion07/capa_datos_persona/logger_base.py
+++ Python/Semana 8 (22-05 al 26-05)/Leccion07/capa_datos_persona/logger_base.py
@@ -1,5 +1,11 @@
 import logging as log
 
 # Llamamos una configuración básica
-log.basicConfig(level=log.DEBUG)
+if __name__ == '__main__':
+    log.basicConfig(level=log.DEBUG)
+    log.debug('Mensaje a nivel debug')
+    log.info('Mensaje a nivel info')
+    log.warning('Mensaje a nivel warning')
+    log.error('Mensaje a nivel error')
+    log.critical('Mensaje a nivel critical')
  
