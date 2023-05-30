2415dc2e304ea5c7497fda060c1bfd68ec5db991
NadiaGonzalez27 <nadiagonzalez206@gmail.com>
2023-05-30 10:58:12 -0300

    Clase 7- Video 7.4
    Manejo de interfaces

---
+++ Java/Semana 8 (22-05 al 26-05)/Leccion06/Interfaces/src/accesodatos/ImplementacionOracle.java
@@ -0,0 +1,26 @@
+
+package accesodatos;
+
+public class ImplementacionOracle implements IAccesoDatos{
+
+    @Override
+    public void insertar() {
+        System.out.println("Insertar desde Oracle");
+    }
+
+    @Override
+    public void listar() {
+        System.out.println("Listar desde Oracle");
+    }
+
+    @Override
+    public void actualizar() {
+        System.out.println("Actualizar desde Oracle");
+    }
+
+    @Override
+    public void eliminar() {
+        System.out.println("Eliminar desde Oracle");
+    }
+    
+}
