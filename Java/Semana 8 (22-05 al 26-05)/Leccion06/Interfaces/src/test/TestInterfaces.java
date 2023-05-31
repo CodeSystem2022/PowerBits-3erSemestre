bf1e5e2ea6b78c8431529f523c50feec5d67376a
juaniM4c <juanimachuca1@gmail.com>
2023-05-31 10:55:05 -0300

    Clase 7 - Video 7.5 (Parte 2)
    Pruebas Java

--- Java/Semana 8 (22-05 al 26-05)/Leccion06/Interfaces/src/test/TestInterfaces.java
+++ Java/Semana 8 (22-05 al 26-05)/Leccion06/Interfaces/src/test/TestInterfaces.java
@@ -7,6 +7,15 @@ import accesodatos.*;
 public class TestInterfaces {
     public static void main(String[] args) {
         IAccesoDatos datos = new ImplementacionMySql();
+        //datos.listar();
+        
+        //imprimir(datos);
+        datos = new ImplementacionOracle();
+        //datos.listar();
+        imprimir(datos);
+    }
+    
+    public static void imprimir(IAccesoDatos datos){
         datos.listar();
     }
 }
