2e630f525417e54cdd3730eef37dd37b8fcf3782
Nico25Mac <112595518+Nico25Mac@users.noreply.github.com>
2023-05-31 15:59:01 -0300

    Clase 8 - Video 8.3 (Parte 2)
    Creación de la Clase Conexión: Parte 2

--- Python/Semana 8 (22-05 al 26-05)/Leccion07/capa_datos_persona/conexion.py
+++ Python/Semana 8 (22-05 al 26-05)/Leccion07/capa_datos_persona/conexion.py
@@ -1,3 +1,9 @@
+import psycopg2 as bd
+# psycopg2 as bd otra manera de importar el psycopg2
+from logger_base import log
+import sys
+
+
 class Conexion:
     _DATABASE = 'test_bd'
     _USERNAME = 'postgres'
@@ -6,3 +12,21 @@ class Conexion:
     _HOST = '127.0.0.1'
     _conexion = None
     _cursor = None
+
+    @classmethod
+    def obtenerConexion(cls):
+        if cls._conexion is None:
+            try:
+                cls._conexion = bd.connect(host=cls._HOST,
+                                           user=cls._USERNAME,
+                                           password=cls._PASSWORD,
+                                           port=cls._DB_PORT,
+                                           database=cls._DATABASE)
+                log.debug(f'Conexión exitosa: {cls._conexion}')
+                return cls._conexion
+            except Exception as e:
+                log.error(f'Ocurrió un error: {e}')
+                sys.exit()
+            else:
+                return cls._conexion
+
