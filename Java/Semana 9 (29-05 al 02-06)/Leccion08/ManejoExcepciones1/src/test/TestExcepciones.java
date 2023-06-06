879704eae053ae292c9ff75091d30d1c6d34c605
Nico25Mac <112595518+Nico25Mac@users.noreply.github.com>
2023-06-06 15:56:52 -0300

    Clase 8 - Video 8.3 Parte (2,3,4)
    Comenzamos Con Excepciones: Parte 2, 3, 4.

--- Java/Semana 9 (29-05 al 02-06)/Leccion08/ManejoExcepciones1/src/test/TestExcepciones.java
+++ Java/Semana 9 (29-05 al 02-06)/Leccion08/ManejoExcepciones1/src/test/TestExcepciones.java
@@ -2,7 +2,13 @@ package test;
 
 public class TestExcepciones {
     public static void main(String[] args) {
-        int resultado = 10/0;
-        System.out.println("resultado = " + resultado);
+        int resultado = 0;
+//        try{
+            resultado = 10/0;
+//        } catch(Exception e){
+//            System.out.println("Ocurrió un Error");
+//            e.printStackTrace(System.out); //Se conoce como la pila de excepciones          
+//        }
+        System.out.println("La variable resultado tiene como valor: "+resultado);
     }
 }
