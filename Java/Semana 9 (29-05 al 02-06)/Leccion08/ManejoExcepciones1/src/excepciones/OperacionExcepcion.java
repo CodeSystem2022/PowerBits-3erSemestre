2e6b69402b109699bdb7ca580a5d9233ee6e787d
Lucas Mendez <112028014+LucasMendez11@users.noreply.github.com>
2023-06-06 18:41:27 -0300

    Clase 8 - 8.4 Creamos nuestra propia excepción - Parte 1

---
+++ Java/Semana 9 (29-05 al 02-06)/Leccion08/ManejoExcepciones1/src/excepciones/OperacionExcepcion.java
@@ -0,0 +1,10 @@
+
+package excepciones;
+
+
+public class OperacionExcepcion extends Exception{
+    // Constructor
+    public OperacionExcepcion(String mensaje){
+        super(mensaje); 
+    }
+}
--- Java/Semana 9 (29-05 al 02-06)/Leccion08/ManejoExcepciones1/src/test/TestExcepciones.java
+++ Java/Semana 9 (29-05 al 02-06)/Leccion08/ManejoExcepciones1/src/test/TestExcepciones.java
@@ -3,12 +3,14 @@ package test;
 public class TestExcepciones {
     public static void main(String[] args) {
         int resultado = 0;
-//        try{
+        try{
             resultado = 10/0;
-//        } catch(Exception e){
-//            System.out.println("Ocurrió un Error");
-//            e.printStackTrace(System.out); //Se conoce como la pila de excepciones          
-//        }
+        } catch(Exception e){
+            System.out.println("Ocurrió un Error");
+            e.printStackTrace(System.out); //Se conoce como la pila de excepciones          
+        }
         System.out.println("La variable resultado tiene como valor: "+resultado);
+        
+        
     }
 }
