e64061657ad21c145c9cfca275ebf83abe0cc060
IviiGonzalez <112676298+IviiGonzalez@users.noreply.github.com>
2023-06-06 15:21:50 -0300

    Clase 8 - Video 8.3
    Comenzamos con Excepciones - Parte 1

---
+++ Java/Semana 9 (29-05 al 02-06)/Leccion08/ManejoExcepciones1/src/test/TestExcepciones.java
@@ -0,0 +1,8 @@
+package test;
+
+public class TestExcepciones {
+    public static void main(String[] args) {
+        int resultado = 10/0;
+        System.out.println("resultado = " + resultado);
+    }
+}
