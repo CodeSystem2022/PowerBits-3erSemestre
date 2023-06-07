aa0ec8c5da0e219f41bef607ef9299be7ef3d008
juaniM4c <juanimachuca1@gmail.com>
2023-06-07 09:40:11 -0300

    Clase 8 - Video 8.3 (Parte 1)
    Creación de la Clase Conexión: Parte 1

---
+++ Python/Semana 8 (22-05 al 26-05)/Leccion07/capa_datos_persona/conexion.py
@@ -0,0 +1,8 @@
+class Conexion:
+    _DATABASE = 'test_bd'
+    _USERNAME = 'postgres'
+    _PASSWORD = 'admin'
+    _DB_PORT = '5432'
+    _HOST = '127.0.0.1'
+    _conexion = None
+    _cursor = None
