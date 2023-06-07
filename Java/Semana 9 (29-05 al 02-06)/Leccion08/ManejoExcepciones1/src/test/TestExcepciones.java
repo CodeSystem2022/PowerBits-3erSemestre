b217bf03a90a3351301f9af206adc82c024c4c4d
Nico25Mac <112595518+Nico25Mac@users.noreply.github.com>
2023-06-07 16:03:33 -0300

    Clase 8 - Video 8.6
    Agregamos el finally y catch

--- Java/Semana 9 (29-05 al 02-06)/Leccion08/ManejoExcepciones1/src/test/TestExcepciones.java
+++ Java/Semana 9 (29-05 al 02-06)/Leccion08/ManejoExcepciones1/src/test/TestExcepciones.java
@@ -1,16 +1,23 @@
 package test;
 
 import static aritmetica.Aritmetica.division;
+import excepciones.OperacionExcepcion;
 
 public class TestExcepciones {
     public static void main(String[] args) {
         int resultado = 0;
         try{
             resultado = division(10, 0);
+        } catch(OperacionExcepcion e){
+            System.out.println("Ocurrió un error de tipo OperacionException");
+            System.out.println(e.getMessage());
         } catch(Exception e){
             System.out.println("Ocurrió un Error");
             e.printStackTrace(System.out); //Se conoce como la pila de excepciones
             System.out.println(e.getMessage());
+        }
+         finally{
+            System.out.println("Se realizo la division entre cero");
         }
         System.out.println("La variable resultado tiene como valor: "+resultado);
     }
