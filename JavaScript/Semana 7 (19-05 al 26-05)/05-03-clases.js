fd6f95c43a6bb2c990914853e33af9cee67fccc2
NadiaGonzalez27 <nadiagonzalez206@gmail.com>
2023-06-07 14:50:05 -0300

    Clase 8-Video 8.4-Palabra static

--- JavaScript/Semana 7 (19-05 al 26-05)/05-03-clases.js
+++ JavaScript/Semana 7 (19-05 al 26-05)/05-03-clases.js
@@ -2,14 +2,14 @@
 
 class Persona{ //Clase padre
 
-    static contadorObjetosPersona = 0; //Atributo estático
-    email = 'Valor default email'; //Atributo no estático
+    static contadorPersonas = 0; //Atributo estático
+    //email = 'Valor default email'; //Atributo no estático
     
     constructor(nombre, apellido){
         this._nombre = nombre;
         this._apellido = apellido;
-        Persona.contadorObjetosPersona++;
-        console.log('Se incrementa el contador: '+Persona.contadorObjetosPersona);
+        this.idPersona = ++Persona.contadorPersonas;
+        //console.log('Se incrementa el contador: '+Persona.contadorObjetosPersona);
     }
 
     get nombre(){
@@ -35,7 +35,7 @@ class Persona{ //Clase padre
     }
 
     nombreCompleto(){
-        return this._nombre+' '+this._apellido;
+        return this.idPersona+' '+this._nombre+' '+this._apellido;
     }
     //Sobreescribiendo el método de la clase padre (Objetc)
     toString(){ // Regresa un String
@@ -114,3 +114,7 @@ console.log(Empleado.contadorObjetosPersona);
 console.log(persona1.email);
 console.log(empleado1.email);
 //console.log(Persona.email); No puede acceder desde la clase
+console.log(persona1.toString());
+console.log(persona2.toString());
+console.log(empleado1.toString());
+console.log(Persona.contadorPersonas)
