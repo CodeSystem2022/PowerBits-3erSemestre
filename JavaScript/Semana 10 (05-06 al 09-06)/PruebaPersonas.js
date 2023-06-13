236e8632eddc3d207249e6be8557bd7cc9694a44
Héctor Fonzalida <hectorfonzalida68@gmail.com>
2023-06-13 15:58:33 -0300

    Clase 9 - Video 9.6
    Prueba clase "Persona"

--- JavaScript/Semana 10 (05-06 al 09-06)/PruebaPersonas.js
+++ JavaScript/Semana 10 (05-06 al 09-06)/PruebaPersonas.js
@@ -38,11 +38,7 @@ class Persona{
     }
 
     toString(){
-        return `
-        ${this._idPersona} 
-        ${this._nombre} 
-        ${this._apellido} 
-        ${this._edad}`;
+        return `${this._idPersona} ${this._nombre} ${this._apellido} ${this._edad}`;
     }
 }
 
@@ -104,3 +100,9 @@ class Cliente extends Persona{
     }
 }
 
+//Prueba clase Persona
+let persona1 = new Persona('Juan', 'Perez', 32);
+console.log(persona1.toString());
+
+let persona2 = new Persona('Carla', 'Ortega', 22);
+console.log(persona2.toString());
