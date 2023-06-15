432648a0ad472718322f5192f4163f6c84361259
NadiaGonzalez27 <nadiagonzalez206@gmail.com>
2023-06-14 23:07:23 -0300

    Clase 9- Video 9.7- Agregamos try catch para las excepciones

--- Java/Semana 10 (04-06 al 09-06)/Leccion09/CalculadoraUTN/CalculadoraUTN.java
+++ Java/Semana 10 (04-06 al 09-06)/Leccion09/CalculadoraUTN/CalculadoraUTN.java
@@ -14,44 +14,47 @@ public class CalculadoraUTN {
                     4. Division
                     5. Salir""");
             System.out.print("Operacion a realizar? ");
-            var operacion = Integer.parseInt(entrada.nextLine());
 
-            if (operacion >= 1 && operacion <= 4) {
-                System.out.println("Digite el valor para el operando1: ");
-                var operando1 = Integer.parseInt(entrada.nextLine());
-                System.out.println("Digite el valor para el operando2: ");
-                var operando2 = Integer.parseInt(entrada.nextLine());
+            try {
+                var operacion = Integer.parseInt(entrada.nextLine());
+                if (operacion >= 1 && operacion <= 4) {
+                    System.out.println("Digite el valor para el operando1: ");
+                    var operando1 = Integer.parseInt(entrada.nextLine());
+                    System.out.println("Digite el valor para el operando2: ");
+                    var operando2 = Integer.parseInt(entrada.nextLine());
 
-                int resultado;
-                switch (operacion) {
-                    case 1 -> { //Suma
-                        resultado = operando1 + operando2;
-                        System.out.println("Resultado de la suma: " + resultado);
-                    }
-                    case 2 -> { //Resta
-                        resultado = operando1 - operando2;
-                        System.out.println("Resultado de la resta: " + resultado);
-                    }
-                    case 3 -> { //Multiplicación
-                        resultado = operando1 * operando2;
-                        System.out.println("Resultado de la multiplicacion: " + resultado);
-                    }
-                    case 4 -> { //División
-                        resultado = operando1 / operando2;
-                        System.out.println("Resultado de la division: " + resultado);
-                    }
-                    default -> System.out.println("Opción erronea: " + operacion);
-                } //Fin switch
-            } //Fin if
-            else if (operacion == 5) {
-                System.out.println("Hasta pronto...");
-                break; //Rompe el ciclo y sale
+                    int resultado;
+                    switch (operacion) {
+                        case 1 -> { //Suma
+                            resultado = operando1 + operando2;
+                            System.out.println("Resultado de la suma: " + resultado);
+                        }
+                        case 2 -> { //Resta
+                            resultado = operando1 - operando2;
+                            System.out.println("Resultado de la resta: " + resultado);
+                        }
+                        case 3 -> { //Multiplicación
+                            resultado = operando1 * operando2;
+                            System.out.println("Resultado de la multiplicacion: " + resultado);
+                        }
+                        case 4 -> { //División
+                            resultado = operando1 / operando2;
+                            System.out.println("Resultado de la division: " + resultado);
+                        }
+                        default -> System.out.println("Opción erronea: " + operacion);
+                    } //Fin switch
+                } //Fin if
+                else if (operacion == 5) {
+                    System.out.println("Hasta pronto...");
+                    break; //Rompe el ciclo y sale
+                } else {
+                    System.out.println("Opción erronea: " + operacion);
+                }
+                //Imprimimos un salto de línea antes de repetir el menú
+                System.out.println();
+            } catch (Exception e){ //Fin try, comienzo del catch
+                System.out.println("Ocurrio un error: "+e.getMessage());
             }
-            else {
-                System.out.println("Opción erronea: " + operacion);
-            }
-            //Imprimimos un salto de línea antes de repetir el menú
-            System.out.println();
         } //Fin while
     } //Fin main
 } //Fin clase
