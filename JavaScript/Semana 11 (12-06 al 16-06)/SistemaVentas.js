60d41423aef83dde41b772e604f16abdc8b8054e
NadiaGonzalez27 <nadiagonzalez206@gmail.com>
2023-06-20 14:34:59 -0300

    Clase 10- Video 10.2- Creamos clase Producto

---
+++ JavaScript/Semana 11 (12-06 al 16-06)/SistemaVentas.js
@@ -0,0 +1,33 @@
+class Producto{
+    static contadorProductos = 0;
+
+    constructor(nombre, precio){
+        this._idProducto = ++Producto.contadorProductos;
+        this._nombre = nombre;
+        this._precio = precio;
+    }
+
+    get idProducto(){
+        return this._idProducto;
+    }
+
+    get nombre(){
+        return this._nombre;
+    }
+
+    set nombre(nombre){
+        this._nombre = nombre;
+    }
+
+    get precio(){
+        return this._precio;
+    }
+
+    set precio(precio){
+        this._precio = precio;
+    }
+
+    toString(){ //Template Literals: Nos permite insertar código dinamicamente
+        return `idPrducto: ${this._idProducto}, nombre: ${this._nombre}, precio: ${this._precio}`;
+    }
+} //Fin de la clase Producto
