622ccf5ffb7ddb7af620870631032c45478f4ca3
Héctor Fonzalida <hectorfonzalida68@gmail.com>
2023-06-21 01:24:13 -0300

    Clase 10 - Video 10.2
    Obtener una conexión a partir de Pool (Video 1).

--- Python/Semana 8 (22-05 al 26-05)/Leccion07/capa_datos_persona/conexion.py
+++ Python/Semana 8 (22-05 al 26-05)/Leccion07/capa_datos_persona/conexion.py
@@ -26,7 +26,19 @@ class Conexion:
     def obtenerPool(cls):
         if cls._pool is None:
             try:
-                cls._pool = pool.SimpleConnectionPool()
+                cls._pool = pool.SimpleConnectionPool(cls._MIN_CON,
+                                                      cls._MAX_CON,
+                                                      host=cls._HOST,
+                                                      user=cls._USERNAME,
+                                                      password=cls._PASSWORD,
+                                                      port=cls._DB_PORT,
+                                                      database=cls._DATABASE)
+                log.debug(f'Creación del pool exitosa: {cls._pool}')
+            except Exception as e:
+                log.error(f'Ocurrió un error al obtener el pool: {e}')
+                sys.exit()
+        else:
+            return cls._pool
 
 if __name__ == '__main__':
     pass
