daf36238738dfb6ff20c57f83b7b56a795c3da38
juaniM4c <juanimachuca1@gmail.com>
2023-07-04 18:08:04 -0300

    Clase 11 - Video 11.2

--- Python/Semana 8 (22-05 al 26-05)/Leccion07/capa_datos_persona/conexion.py
+++ Python/Semana 8 (22-05 al 26-05)/Leccion07/capa_datos_persona/conexion.py
@@ -47,13 +47,21 @@ class Conexion:
         cls.obtenerPool().putconn(conexion)
         log.debug(f'Regresamos la conexion del pool: {conexion}')
 
+    @classmethod
+    def cerrarConexiones(cls):
+        cls.obtenerPool().closeall()
+
 
         
 if __name__ == '__main__':
     conexion1 = Conexion.obtenerConexion()
+    Conexion.liberarConexion(conexion1)
     conexion2 = Conexion.obtenerConexion()
+    Conexion.liberarConexion(conexion2)
     conexion3 = Conexion.obtenerConexion()
+    Conexion.liberarConexion(conexion3)
     conexion4 = Conexion.obtenerConexion()
     conexion5 = Conexion.obtenerConexion()
+    conexion6 = Conexion.obtenerConexion()
 
 
