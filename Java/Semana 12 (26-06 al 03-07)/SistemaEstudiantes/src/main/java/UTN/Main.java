0cd7d203dc74d40dd5b8c42818867674ef12306c
Antonella arraez <antooarraez2001@gmail.com>
2023-07-04 17:38:38 -0300

    Creamos la clase conexion PT2

--- Java/Semana 12 (26-06 al 03-07)/SistemaEstudiantes/src/main/java/UTN/Main.java
+++ Java/Semana 12 (26-06 al 03-07)/SistemaEstudiantes/src/main/java/UTN/Main.java
@@ -1,19 +1,12 @@
 package UTN;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import UTN.conexion.Conexion;
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Intro with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Mayús+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Mayús+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
-    }
-}
+        var conexion = Conexion.getConnection();
+        if(conexion != null)
+            System.out.println("Conexión exitosa: "+conexion);
+        else
+            System.out.println("Error al conectarse");
+    } //Fin main
+}// Fin clase
