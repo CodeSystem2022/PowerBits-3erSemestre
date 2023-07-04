136e24843b5370ba0c42dc1db12e73e2533c9c38
Nico25Mac <112595518+Nico25Mac@users.noreply.github.com>
2023-07-04 18:56:18 -0300

    Clase 11 - Video 11.3.2 y 11.4.1
    11.3 - Creamos la Clase CursorDelPool -> Parte 2.
    11.4 - Pruebas del CursorDelPool -> Parte 1

--- Python/Semana 8 (22-05 al 26-05)/Leccion07/capa_datos_persona/cursor_del_pool.py
+++ Python/Semana 8 (22-05 al 26-05)/Leccion07/capa_datos_persona/cursor_del_pool.py
@@ -12,3 +12,21 @@ class CursorDelPool:
         self._conexion = Conexion.obtenerConexion()
         self._cursor = self._conexion.cursor()
         return self._cursor
+
+    def __exit__(self, tipo_exception, valor_exception, detalle_exception):
+        log.debug('Se ejecuta el método exit')
+        if valor_exception:
+            self._conexion.rollback()
+            log.debug(f'Ocurrió una exception: {valor_exception}')
+        else:
+            self._conexion.commit()
+            log.debug('Commit de la transacción')
+        self._cursor.close()
+        Conexion.liberarConexion(self._conexion)
+
+
+if __name__ == '__main__':
+    with CursorDelPool() as cursor:
+        log.debug('Dentro del bloque with')
+        cursor.execute('SELECT * FROM persona')
+        log.debug(cursor.fetchall())
