0a523e1eadbb6fab91d216026d6a4aa863f7973b
juaniM4c <juanimachuca1@gmail.com>
2023-07-04 21:47:07 -0300

    Clase 12 - Video 12.2 (Parte 2)

--- Java/Semana 12 (26-06 al 03-07)/SistemaEstudiantes/src/main/java/UTN/datos/EstudianteDAO.java
+++ Java/Semana 12 (26-06 al 03-07)/SistemaEstudiantes/src/main/java/UTN/datos/EstudianteDAO.java
@@ -2,6 +2,8 @@ package UTN.datos;
 
 import UTN.dominio.Estudiante;
 
+import static UTN.conexion.Conexion.getConnection;
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
@@ -14,6 +16,8 @@ public class EstudianteDAO {
         //Creamos algunos objetos que son necesarios para comunicarnos con la base de datos.
         PreparedStatement ps; //Envía la sentencia a la base de datos.
         ResultSet rs; //Obtenemos el resultado de la base de datos.
-        
+        // Creamos un objeto de tipo conexion
+        Connection con = getConnection();
+        String sql = "SELECT * FROM "
     }
 }
